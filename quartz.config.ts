46a860040186f3c2473086cb90c9a357c70d82a6
eduardo.florencio <eduardo.florencio@linx.com.br>
2026-01-14 16:19:26 -0300

    fix: update CrawlLinks plugin configuration for relative link resolution and disable pretty links

--- quartz.config.ts
+++ quartz.config.ts
@@ -71,7 +71,7 @@ const config: QuartzConfig = {
       }),
       Plugin.GitHubFlavoredMarkdown(),
       Plugin.TableOfContents(),
-      Plugin.CrawlLinks({ markdownLinkResolution: "shortest", prettyLinks: true }),
+      Plugin.CrawlLinks({ markdownLinkResolution: "relative", prettyLinks: false }),
       Plugin.Description(),
       Plugin.Latex({ renderEngine: "katex" }),
     ],
